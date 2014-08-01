753c179fdeea71871aea7f21c08129169588f19f
animasola <annima.sola@gmail.com>
2014-08-01 20:49:38 +0300

    ticket:5 tests

--- my_info/tests/views_tests.py
+++ my_info/tests/views_tests.py
@@ -1,8 +1,15 @@
 #-*- coding:utf-8 -*-
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.core.files.uploadedfile import SimpleUploadedFile
+from StringIO import StringIO
+from datetime import datetime
+
+from PIL import Image
+from django_any import any_model
 
 from my_info.models import Contact, LoggedRequest
+from my_info.forms import ContactForm
 
 
 class MyInfoViewsTests(TestCase):
@@ -13,12 +20,8 @@ class MyInfoViewsTests(TestCase):
         self.field_names = Contact._meta.get_all_field_names()
 
     def test_page_content(self):
-        # should be asking to login
-        response = self.client.get(reverse('home'))
-        self.assertEquals(response.status_code, 302)
-        self.client.login(username='admin', password='admin')
-        response = self.client.get(reverse('home'))
 
+        response = self.client.get(reverse('home'))
         # all field values except "id" should be present at the home page
         for field_name in [field for field in self.field_names if field != 'id']:
             if field_name == 'birth_date':
@@ -73,3 +76,59 @@ class MyInfoViewsTests(TestCase):
         self.assertEquals(
             LoggedRequest.objects.all().count(),
             22)
+
+
+class ContactsEditPageTestCase(TestCase):
+    fixtures = ['initial_data.json']
+
+    def setUp(self):
+        self.current_instance = Contact.objects.all()[0]
+        self.dummy_file = StringIO()
+        self.image = Image.new("RGBA", size=(40, 60), color=(256, 239, 114))
+        self.image.save(self.dummy_file, 'png')
+        self.dummy_file.name = 'test_%s.png' % datetime.now().microsecond
+        self.dummy_file.seek(0)
+        # self.form_data = any_model(Contact, photo="")
+        # self.post_data = self.form_data.__dict__
+
+    def test_get(self):
+        # need to be authenticated
+        response = self.client.get(reverse('edit_contacts'))
+        self.assertEquals(response.status_code, 302)
+
+        self.client.login(username='admin', password='admin')
+        response = self.client.get(reverse('edit_contacts'))
+        self.assertEquals(response.status_code, 200)
+        # should only one instance in Contact model
+        self.assertEquals(
+            Contact.objects.all().count(), 1)
+        # checking each field's data is present in response
+        for field_name, value in self.current_instance.__dict__.iteritems():
+            if field_name in ['_state', 'id']:
+                continue
+            self.assertContains(response, value, status_code=200)
+
+    def test_form(self):
+        new_form_data = any_model(Contact, birth_date='2014-12-10', photo='')
+        post_dict = new_form_data.__dict__
+        file_dict = {
+            'photo': SimpleUploadedFile(self.dummy_file.name, self.dummy_file.read())
+        }
+        form = ContactForm(
+            post_dict, file_dict, instance=self.current_instance)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        # data on the main page should be changed now
+        response = self.client.get(reverse('home'))
+        for key, value in post_dict.iteritems():
+            if key == 'birth_date':
+                self.assertContains(
+                    response,
+                    value.strftime('%b. %d, %Y'),
+                    status_code=200)
+            elif key in ['extra_contacts', 'bio']:
+                self.assertContains(
+                    response,
+                    value[0],
+                    status_code=200)
