c34c689d278a3741e5cb3113194fa2f0e69b952a
animasola <annima.sola@gmail.com>
2014-08-01 19:44:59 +0300

    ticket:1 Contacts model, fixtures, migrations

--- my_info/models.py
+++ my_info/models.py
@@ -1,3 +1,17 @@
+#-*- coding:utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name="Name")
+    last_name = models.CharField(max_length=75, verbose_name="Last Name")
+    birth_date = models.DateField(
+        auto_now=False, auto_now_add=False, verbose_name="Date Of Birth")
+    bio = models.TextField(verbose_name="Short Biography")
+    email = models.EmailField(max_length=75, verbose_name="Email Address")
+    jabber = models.CharField(max_length=50)
+    skype = models.CharField(max_length=50)
+    extra_contacts = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return "%s %s" % (self.last_name, self.first_name)
