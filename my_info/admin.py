fe4db99db528eca272aa0479950d9a6d79d066df
animasola <annima.sola@gmail.com>
2014-08-09 14:05:07 +0300

    ticket:13 add logged requests model to admin site

--- my_info/admin.py
+++ my_info/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from my_info.models import Contact
+from my_info.models import Contact, LoggedRequest
 
 
 class ContactAdmin(admin.ModelAdmin):
     search_fields = ['last_name', 'name']
 
 
+class LoggedRequestAdmin(admin.ModelAdmin):
+    list_display = ['url', 'request_type', 'timestamp', 'priority']
+
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(LoggedRequest, LoggedRequestAdmin)
