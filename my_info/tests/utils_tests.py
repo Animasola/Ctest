78b6ae12308673081b96ea518a6fbae18a5a63f8
animasola <annima.sola@gmail.com>
2014-08-09 12:49:44 +0300

    ticket:10 tests

--- my_info/tests/utils_tests.py
+++ my_info/tests/utils_tests.py
@@ -8,7 +8,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from StringIO import StringIO
 
-from my_info.models import Contact
+from my_info.models import Contact, LoggedRequest, ModelChangeLog
 
 
 class ContextProcessorTestCase(TestCase):
@@ -69,3 +69,36 @@ class CommandsTestCase(TestCase):
                 model.model_class().objects.count()),
                 stdout.getvalue()
             )
+
+
+class SignalModelLoggerTestCase(TestCase):
+
+    fixtures = ['initial_data.json']
+
+    def setUp(self):
+        # creating object
+        self.request = LoggedRequest.objects.create()
+
+    def test_creation(self):
+        log_record = ModelChangeLog.objects.all().order_by('-timestamp')[0]
+        self.assertEquals(
+            log_record.model_name, self.request.__class__.__name__)
+        self.assertEquals(log_record.app_label, self.request._meta.app_label)
+        self.assertEquals(log_record.action, ModelChangeLog.CREATED)
+
+    def test_alter_delete(self):
+        # change url field
+        self.request.url = 'google.com.ua'
+        self.request.save()
+        log_record = ModelChangeLog.objects.all().order_by('-timestamp')[0]
+        self.assertEquals(
+            log_record.model_name, self.request.__class__.__name__)
+        self.assertEquals(log_record.app_label, self.request._meta.app_label)
+        self.assertEquals(log_record.action, ModelChangeLog.ALTERED)
+        # delete record
+        self.request.delete()
+        log_record = ModelChangeLog.objects.all().order_by('-timestamp')[0]
+        self.assertEquals(
+            log_record.model_name, self.request.__class__.__name__)
+        self.assertEquals(log_record.app_label, self.request._meta.app_label)
+        self.assertEquals(log_record.action, ModelChangeLog.DELETED)
