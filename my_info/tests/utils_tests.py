94c2790766b30133db5fd416fbaabb282b2c20c4
animasola <annima.sola@gmail.com>
2014-08-09 11:28:14 +0300

    ticket:9 tests

--- my_info/tests/utils_tests.py
+++ my_info/tests/utils_tests.py
@@ -3,7 +3,10 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.template import Template, Context
+from django.core.management import call_command
 from django.contrib.contenttypes.models import ContentType
+
+from StringIO import StringIO
 
 from my_info.models import Contact
 
@@ -47,3 +50,22 @@ class TemplateTagsTestCase(TestCase):
             args=(self.current_instance.id,)
         )
         self.failUnlessEqual(template.render(context).strip(), expected_url)
+
+
+class CommandsTestCase(TestCase):
+
+    fixtures = ['initial_data.json']
+
+    def test_all_models_command(self):
+        stdout = StringIO()
+        stderr = StringIO()
+        call_command('models_info', stderr=stderr, stdout=stdout)
+        for out, err in zip(stdout.readlines(), stderr.readlines()):
+            self.assertEquals(err, 'Error: %s' % out)
+        for model in ContentType.objects.all():
+            self.assertIn('%s: %s - %s' % (
+                model.app_label,
+                model.model,
+                model.model_class().objects.count()),
+                stdout.getvalue()
+            )
